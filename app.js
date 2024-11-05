c3980a4cdd55d08a74c46e96e34348ba60dd107e
patleserdev <leserpat@hotmail.com>
2024-11-05 16:39:28 +0100

    update cors

--- app.js
+++ app.js
@@ -15,21 +15,25 @@ const fileUpload = require('express-fileupload');
 
 
 var app = express();
-  // Configuration CORS
-
-  const corsOptions = {
-    origin: ['https://blog-postgre-frontend.vercel.app','http://localhost:3001'], 
-    // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    optionsSuccessStatus: 200 
+app.use(fileUpload());
+// Configuration CORS
+const corsOptions = {
+  origin: 'https://blog-postgre-frontend.vercel.app/',
+  // origin: ['http://localhost:3001'],
+  // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  // credentials: true,  // Permettre l'envoi des cookies ou des headers d'authentification
+  optionsSuccessStatus: 200
 };
 
+// Appliquer CORS à toutes les routes
 app.use(cors(corsOptions));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(fileUpload());
+
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
 app.use('/comments', commentsRouter);
